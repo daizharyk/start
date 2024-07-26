89243950d3393c8250281c9fb365a56587271518
daizharyk <aizharyk76@gmail.com>
2024-07-26 12:28:51 +0500

    добавил scrollIntoView

--- js.js
+++ js.js
@@ -5,7 +5,14 @@ const contacts = document.getElementById('contacts')
 console.log(allArticles);
 console.log(contacts);
 
-let articleCount = 2;
+let articleCount = 3;
 
 allArticles.querySelector('a').textContent = `Все статьи (${articleCount})`;
 
+let lastArticlesSection = document.querySelectorAll(".section_element")[
+  document.querySelectorAll(".section_element").length -1
+];
+
+allArticles.addEventListener("click", ()=>{
+  lastArticlesSection.scrollIntoView({ behavior: "smooth" });
+});
