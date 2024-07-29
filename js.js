2b2ca1fde873cce9e1f805bc6f87bbf6ac76807b
daizharyk <aizharyk76@gmail.com>
2024-07-29 15:10:27 +0500

    Динамическое создание элементов урок 5

--- js.js
+++ js.js
@@ -9,7 +9,7 @@ let articleCount = 3;
 
 allArticles.querySelector('a').textContent = `Все статьи (${articleCount})`;
 
-let lastArticlesSection = document.querySelector(".article_section")
+let articlesSection = document.querySelector(".article_section")
 
 
 
@@ -19,7 +19,7 @@ let scrollIntoElement = (el) =>{
 
 allArticles.addEventListener("click", (e)=>{
   e.preventDefault();
-  scrollIntoElement(lastArticlesSection);
+  scrollIntoElement(articlesSection);
 });
 
 
@@ -29,3 +29,19 @@ contacts.addEventListener("click",(e) =>{
   e.preventDefault();
   scrollIntoElement(contactsSection);
 });
+
+
+let articles = [
+  { name: "JavaScript", text: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили." }
+  { name: "DOM", text: "DOM — это независящий от платформы и языка программный интерфейс, позволяющий программам и скриптам получить доступ к содержимому HTML-, XHTML- и XML-документов, а также изменять содержимое, структуру и оформление таких документов. "}
+];
+
+let articleElements = articles.map ((article) =>{
+  let { name, text } = article;
+
+  let articleCard = document.createElement("article");
+  articleCard.className = "section_element";
+  articleCard.innerText = `${name} ${text}`;
+  return articleCard;
+});
+console.log(articleElements);
