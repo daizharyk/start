07dea9cba5f18123e50ade2bd0fd7af6f8ecc375
daizharyk <aizharyk76@gmail.com>
2024-07-29 16:19:24 +0500

    Модуль -4 урок 5 , Динамическое создание элементов

--- js.js
+++ js.js
@@ -32,18 +32,28 @@ contacts.addEventListener("click",(e) =>{
 
 
 let articles = [
-  { name: "JavaScript", text: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили." },
-  { name: "DOM", text: "DOM — это независящий от платформы и языка программный интерфейс, позволяющий программам и скриптам получить доступ к содержимому HTML-, XHTML- и XML-документов, а также изменять содержимое, структуру и оформление таких документов. "},
+  { name: "JavaScript", text: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.", link: "Читать дальше" },
+  { name: "DOM", text: "DOM — это независящий от платформы и языка программный интерфейс, позволяющий программам и скриптам получить доступ к содержимому HTML-, XHTML- и XML-документов, а также изменять содержимое, структуру и оформление таких документов.", link: "Читать дальше"},
 ];
 
 let articleElements = articles.map ((article) =>{
-  let { name, text } = article;
+  let { name, text, link } = article;
 
   let articleCard = document.createElement("article");
   articleCard.className = "section_element";
-  articleCard.innerText = `${name} ${text}`;
+  let title = document.createElement("h2");
+  title.innerText = name;
+  articleCard.append(title);
+  let paragraph = document.createElement("p");
+  paragraph.innerText = text;
+  articleCard.append(paragraph);
+  let linkbt = document.createElement("a");
+  linkbt.setAttribute("href", "frontend.html");
+  linkbt.innerText = link;
+  articleCard.append(linkbt);
   return articleCard;
 });
+
 console.log(articleElements);
 
 
