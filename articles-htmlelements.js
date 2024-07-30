be2c2055d953329da5cb57a599a912f3e186d526
daizharyk <aizharyk76@gmail.com>
2024-07-30 12:10:37 +0500

    Разделение кода , модуль-4 , урок -6

---
+++ articles-htmlelements.js
@@ -0,0 +1,28 @@
+
+
+
+let articles = [
+  { name: "Frontend-разработчик", text: "Отвечает за верхушку айсберга, то есть за ту его часть, которую мывидим. Пользователь заходит на определенный сайт и видит некую картинку.", link: "frontend.html" },
+  { name: "Backend-специалист", text: "Это главный по внутренностям сайта, ответственный за его серверный отдел.", link: "backend.html" },
+  { name: "Fullstack-разработчик", text: "Это универсальный работник.", link: "Fullstack.html"},
+  { name: "JavaScript", text: "JavaScript — мультипарадигменный язык программирования. Поддерживает объектно-ориентированный, императивный и функциональный стили.", link: "Fullstack.html"},
+  { name: "DOM", text: "DOM — это независящий от платформы и языка программный интерфейс, позволяющий программам и скриптам получить доступ к содержимому HTML-, XHTML- и XML-документов, а также изменять содержимое, структуру и оформление таких документов.", link: "Fullstack.html"},
+];
+
+export let articleElements = articles.map ((article) =>{
+  let { name, text, link } = article;
+
+  let articleCard = document.createElement("article");
+  articleCard.className = "section_element";
+  let title = document.createElement("h2");
+  title.innerText = name;
+  articleCard.append(title);
+  let paragraph = document.createElement("p");
+  paragraph.innerText = text;
+  articleCard.append(paragraph);
+  let linkbt = document.createElement("a");
+  linkbt.setAttribute("href", link);
+  linkbt.innerText = "Читать далее";
+  articleCard.append(linkbt);
+  return articleCard;
+});
